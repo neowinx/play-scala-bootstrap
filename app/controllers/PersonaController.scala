7a2dc27770aff1e868ee2c8ecbdcda51cd5d4ded
Pedro Flores <pflores@dncp.gov.py>
2013-10-30 10:27:09 -0300

    Edit record added. Guard Livereload file added

--- app/controllers/PersonaController.scala
+++ app/controllers/PersonaController.scala
@@ -4,16 +4,33 @@ import play.api._
 import play.api.mvc._
 import scala.concurrent.Future
 import models.defaultPersistenceContext._
-import models.Persona
+import models.{Persona}
 import play.api.data._
 import play.api.data.Forms._
+import views.html
+import net.fwbrasil.activate.play.EntityForm
 
 object PersonaController extends Controller {
+
+  val personaForm = EntityForm[Persona](
+    _.nombre -> nonEmptyText,
+    _.fechaNacimiento -> optional(date("yyyy-MM-dd"))
+  )
 
   def index(page: Int, orderBy: Int, filter: String) = Action.async { implicit request =>
     asyncTransactionalChain { implicit ctx =>
       Persona.list(page, orderBy, filter = ("*" + filter + "*")).map {
         page => Ok(views.html.persona.list(page, orderBy, filter))
+      }
+    }
+  }
+
+  def edit(id: String) = Action.async {
+    asyncTransactionalChain { implicit ctx =>
+      asyncById[Persona](id).map { personaOption =>
+        personaOption.map { persona =>
+          Ok(html.persona.edit(id, personaForm.fillWith(persona)))
+        }.getOrElse(NotFound)
       }
     }
   }
