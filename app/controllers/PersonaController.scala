0f5f67bc0079f338cddb72f25dc975bcb7eeb9f7
Pedro Flores <pflores@codelab.com.py>
2013-11-01 16:47:04 -0300

    Finalized basic edition form.

--- app/controllers/PersonaController.scala
+++ app/controllers/PersonaController.scala
@@ -12,6 +12,8 @@ import net.fwbrasil.activate.play.EntityForm
 
 object PersonaController extends Controller {
 
+  val PersonaList = Redirect("/persona")
+
   val personaForm = EntityForm[Persona](
     _.nombre -> nonEmptyText,
     _.fechaNacimiento -> optional(date("yyyy-MM-dd"))
@@ -22,6 +24,24 @@ object PersonaController extends Controller {
       Persona.list(page, orderBy, filter = ("*" + filter + "*")).map {
         page => Ok(views.html.persona.list(page, orderBy, filter))
       }
+    }
+  }
+
+  def create = Action.async {
+    Future.successful(Ok("AAAA"))
+  }
+
+  def save = Action.async { implicit request =>
+    asyncTransactionalChain { implicit ctx =>
+        personaForm.bindFromRequest.fold(
+          formWithErrors =>
+              Future.successful(BadRequest(views.html.persona.edit("",personaForm))),
+          personaData => {
+              personaData.asyncCreateEntity.map { persona =>
+                  PersonaList.flashing("success" -> "Persona has been created")
+              }
+          }
+        )
     }
   }
 
@@ -38,12 +58,25 @@ object PersonaController extends Controller {
   def update(id: String) = Action.async { implicit request =>
     asyncTransactionalChain { implicit ctx =>
       personaForm.bindFromRequest.fold (
-          formWithErrors => Future.successful(BadRequest( "You need to pass a 'xxx' value!" )),
-          { personaOption =>
-              Future.successful(Ok("Yay"))
+          formWithErrors => Future.successful(BadRequest( "Error al actualizar" )),
+          personaData => {
+            personaData.asyncUpdateEntity(id).map { persona =>
+              PersonaList.flashing("success" -> "Persona editada correctamente")
+            }
           }
       )    
     }
+  }
+
+  def delete(id: String) = Action.async {
+      asyncTransactionalChain { implicit ctx =>
+          asyncById[Persona](id).map { personaOption =>
+              personaOption.map { 
+                persona => persona.delete
+              }.getOrElse(NotFound)
+              PersonaList.flashing("success" -> "Persona eliminada correctamente")
+          }
+      }
   }
 
 }
